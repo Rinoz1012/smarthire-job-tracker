2f9078ba68b5ff67eee8b50d8a6498e34b673a6b
Rinoz1012 <144412372+Rinoz1012@users.noreply.github.com>
2026-04-25 18:36:36 +0530

    add firebase configuration

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWy4g32A51a3srrZ13cgTfwJlBocVKcl8",
+  authDomain: "smarthire-77d77.firebaseapp.com",
+  projectId: "smarthire-77d77",
+  storageBucket: "smarthire-77d77.firebasestorage.app",
+  messagingSenderId: "1031680499153",
+  appId: "1:1031680499153:web:cbcc7e275bdfebae56d4f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
